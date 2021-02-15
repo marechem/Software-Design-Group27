b064cb0e2e01254b4f637ef887be1859170a1745
Jerry Lopez <lopez27g@gmail.com>
2021-02-15 10:16:34 -0600

    Added Routes and created the first route to the login page.

---
+++ fuel-quote-app/src/pages/login.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Form, Button, Card, CardBody, Row, Col, Container, FormGroup, FormLabel, FormControl, FormText } from "react-bootstrap";
+
+const LoginPage = () => {
+  return (
+    <Card>
+      <Card.Body>
+        <div className="container">
+          <Form>
+            <Form.Group controlId="formBasicUsername">
+              <Form.Label>Username</Form.Label>
+              <Form.Control type="text" placeholder="Username" />
+              <Form.Text className="text-muted">
+                Please enter your unique username.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </div>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default LoginPage
