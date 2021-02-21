4c6c372077cc1ced9efad1a8d10f3eeb11965fef
marechem <m.vlobos7@gmail.com>
2021-02-20 21:29:26 -0600

    Added Registration Page as well as route to the page

--- fuel-quote-app/src/App.js
+++ fuel-quote-app/src/App.js
@@ -7,10 +7,12 @@ Switch } from "react-router-dom";
 
 //Pages
 import LoginPage from "./pages/login";
+import RegisterPage from "./pages/register";
 
 //Components
 import TopNavbar from "./components/navbar.component";
 import NewFuelQuote from "./components/fuelQuote.component";
+
 
 class App extends Component {
   render() {
@@ -20,6 +22,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={LoginPage} />
           <Route path="/newForm" component={NewFuelQuote} />
+          <Route path="/register" component={RegisterPage} />
         </Switch>
       </Router>
       
---
+++ fuel-quote-app/src/pages/register.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Form, Button, Card, Container, Row, Col } from "react-bootstrap";
+
+const RegisterPage = () => {
+    return (
+        <Container className="mt-5 pt-5">
+          <Card>
+              <Card.Body>
+                  <div className="m-4">
+                      <Form>
+                          <Form.Group controlId="formGroupUsername">
+                              <Form.Label>Create New Username</Form.Label>
+                              <Form.Control type="username" placeholder="Enter username" />
+                            </Form.Group>
+                          <Form.Group controlId="formGroupPassword">
+                             <Form.Label>Create New Password</Form.Label>
+                             <Form.Control type="password" placeholder="Enter Password" />
+                           </Form.Group>
+                       </Form>
+                       <Button variant="primary" type="submit">
+                          Submit
+                       </Button>
+                   </div>
+               </Card.Body>
+           </Card>
+       </Container>
+    );
+};
+
+export default RegisterPage
